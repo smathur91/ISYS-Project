95896e7c33c78f8c32ae67ef8b174f5843889e13
shashaank mathur <mathur.shashaank@gmail.com>
2016-04-03 14:47:31 -0500

    commit 3:00pm

---
+++ WebApplication1/CS.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class CS : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            this.BindGrid();
+        }
+    }
+
+    private void BindGrid()
+    {
+        string strConnString = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(strConnString))
+        {
+            using (SqlCommand cmd = new SqlCommand("SELECT * FROM Customers"))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+                    sda.SelectCommand = cmd;
+                    using (DataTable dt = new DataTable())
+                    {
+                        sda.Fill(dt);
+                        GridView1.DataSource = dt;
+                        GridView1.DataBind();
+                    }
+                }
+            }
+        }
+    }
+
+    protected void OnPageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        GridView1.PageIndex = e.NewPageIndex;
+        this.BindGrid();
+    }
+}
